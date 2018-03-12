115c9bde7a4192196dff473850fd9a54b4142295
DakotaBlackDesign <35580597+DakotaBlackDesign@users.noreply.github.com>
2018-03-12 14:37:46 -0700

    Update wrong-turn.ino

--- wrong-turn.ino
+++ wrong-turn.ino
@@ -1,1 +1,41 @@
+int pulse = 2000;
+float t = 0.05;
+int Que;
+int rate = 0;
+float x = 0;
+int count = 0; 
+void setup() {
+  // put your setup code here, to run once:
+pinMode(9, OUTPUT);
+
+Serial.begin(9600);
+}
+
+void loop() {
+  if (count < 4){
+    t = 0.005;
+    rate = 0;
+    fade();
+    analogWrite(9, rate);
+  }
+  else if (count >= 2 && count < 14){
+   t = 0.01;
+   rate = 0;
+   fade();
+   analogWrite(9, rate);
+  } 
+  else{
+    count = 0;   
+  } 
+}
+
+
+void fade(){
+  x += t;
+  if ( x > 6.28319){
+    count += 1;
+    x = 0;
+    }
+  rate = (127 + 127 * sin(x));  
+}
 
