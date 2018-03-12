87cad04e9f45c6445c6a5e35ea8e1e282acce976
DakotaBlackDesign <35580597+DakotaBlackDesign@users.noreply.github.com>
2018-03-12 16:55:57 -0700

    Update urgent.ino

--- urgent.ino
+++ urgent.ino
@@ -2,17 +2,12 @@ float t = 0.05;
 int rate;
 float x = 0;
 void setup() {
-  // put your setup code here, to run once:
 pinMode(9, OUTPUT);
-
-Serial.begin(9600);
 }
 
 void loop() {
   fade();
-  Serial.println(x);
   analogWrite(9, rate);// turn the LED on (HIGH is the voltage level)
- 
 }
 
 void fade(){
