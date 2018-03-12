b0f68a137709e6167f09455e619b553b28e645ad
DakotaBlackDesign <35580597+DakotaBlackDesign@users.noreply.github.com>
2018-03-12 09:57:24 -0700

    Update overdue.ino

--- overdue.ino
+++ overdue.ino
@@ -1,1 +1,23 @@
+int rate = 200;
+int timer = millis();
+int timer2 = timer;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+}
 
+void loop() {
+  // put your main code here, to run repeatedly:
+  rate = (220 + 200 * sin(millis() / 300));
+  Serial.println(rate);
+  timer2 = millis();
+  if (timer2 - timer > rate){
+    digitalWrite(9, HIGH);// turn the LED on (HIGH is the voltage level)
+  }
+  if (timer2 - timer > 2 * rate){
+    digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
+    timer = millis();
+    timer2 = timer;
+  }
+}
